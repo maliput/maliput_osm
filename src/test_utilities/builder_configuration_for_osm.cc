ce8eec52ea61dfe47d9fe4541f71967a954c734e
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-11-10 13:05:28 -0300

    Adds t_shape_road.osm map. (#43)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- src/test_utilities/builder_configuration_for_osm.cc
+++ src/test_utilities/builder_configuration_for_osm.cc
@@ -83,6 +83,16 @@ std::optional<builder::BuilderConfiguration> GetBuilderConfigurationFor(const st
            1. /* scale_length */,
            kZeroVector,
        }},
+      {"t_shape_road.osm",
+       builder::BuilderConfiguration{
+           maliput::api::RoadGeometryId{"t_shape_road"},
+           utilities::FindOSMResource("t_shape_road.osm"),
+           {0., 0.} /* origin */,
+           5e-2 /* linear_tolerance */,
+           1e-3 /* angular_tolerance */,
+           1. /* scale_length */,
+           kZeroVector,
+       }},
 
   };
 
